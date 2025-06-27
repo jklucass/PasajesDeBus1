ceafa41c28417a33ca8c2b0e6db700d5d3dc3c51
joo04-dot <jos.taborga@duocuc.cl>
2025-06-26 22:17:32 -0400

    extension-3 codigo de bus

---
+++ extensiones-3 codigo pasaje de bus.py
@@ -0,0 +1,8 @@
+def menu():
+    while True:
+        print("\n--- Menú PasajeBus ---")
+        print("1. Ver asientos disponibles")
+        print("2. Comprar pasaje")
+        print("3. Ver pasajes vendidos")
+        print("4. Salir")
+        opcion = input("Seleccione una opción (1-4): ")
